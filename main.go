109d3ef515a09e7437aeeba30ffb59138c7e89ec
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-11-07 05:55:15 +0200

    Customizing responses

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -18,11 +19,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
