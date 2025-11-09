6c5b28196e37900e8718496f94ddd38616258278
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-11-09 11:59:24 +0200

    Template actions and functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/voznyibohdan/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
