558f4aec9d7cabde4ffc81c33dcfd7f215d757cb
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-11-12 14:44:13 +0200

    user routes setup

---
+++ cmd/web/user-handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+
+}
