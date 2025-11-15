891576033ed8e7109e20428a6316e53fdf9a5438
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-11-15 13:10:50 +0200

    Mocking dependencies

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -17,3 +17,59 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, res.status, http.StatusOK)
 	assert.Equal(t, res.body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name       string
+		urlPath    string
+		wantStatus int
+		wantBody   string
+	}{
+		{
+			name:       "Valid ID",
+			urlPath:    "/snippet/view/1",
+			wantStatus: http.StatusOK,
+			wantBody:   "An old silent pond...",
+		},
+		{
+			name:       "Non-existent ID",
+			urlPath:    "/snippet/view/2",
+			wantStatus: http.StatusNotFound,
+		},
+		{
+			name:       "Negative ID",
+			urlPath:    "/snippet/view/-1",
+			wantStatus: http.StatusNotFound,
+		},
+		{
+			name:       "Decimal ID",
+			urlPath:    "/snippet/view/1.23",
+			wantStatus: http.StatusNotFound,
+		},
+		{
+			name:       "String ID",
+			urlPath:    "/snippet/view/foo",
+			wantStatus: http.StatusNotFound,
+		},
+		{
+			name:       "Empty ID",
+			urlPath:    "/snippet/view/",
+			wantStatus: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts.resetClientCookieJar(t)
+
+			res := ts.get(t, tt.urlPath)
+			assert.Equal(t, res.status, tt.wantStatus)
+			assert.StringContains(t, res.body, tt.wantBody)
+		})
+	}
+}
